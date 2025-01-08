1e0841a596302c868414e09c8ee63f6290c95c7d
Dalitso Kasonde <dkasonde11@gmail.com>
2025-01-08 12:13:36 +0200

    initial commit

---
+++ investment_calculator.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main(){
+	var investmentAmount = 1000
+	var expectedReturnRate = 5.5
+	var years = 10
+
+	var futureValue = investmentAmount * (1 + expectedReturnRate / 100)
+}
